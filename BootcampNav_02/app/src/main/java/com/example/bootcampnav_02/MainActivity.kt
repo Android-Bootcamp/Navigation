da51ec8f36072fdd8d2b944da01d717437ee5b06
Freddy Montano <fmontano@pinterest.com>
2021-09-06 15:18:03 -0700

    added new nav stuff

--- BootcampNav_02/app/src/main/java/com/example/bootcampnav_02/MainActivity.kt
+++ BootcampNav_02/app/src/main/java/com/example/bootcampnav_02/MainActivity.kt
@@ -14,17 +14,14 @@ const val INTENT_EXTRAS_KEY_NAME = "intentExtrasKeyName"
 class MainActivity : AppCompatActivity() {
 
     private lateinit var nameTextView: TextView
-    private val editNameRequestCode = 1
 
+    private val startForResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            val name = result.data?.extras?.getString(INTENT_EXTRAS_KEY_NAME) ?: getString(R.string.hi_my_name_is)
 
-    // This is how we would set the callbacks the new way
-    //private val startForResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
-    //    if (result.resultCode == Activity.RESULT_OK) {
-    //        val name = result.data?.extras?.getString(INTENT_EXTRAS_KEY_NAME) ?: getString(R.string.hi_my_name_is)
-
-    //        nameTextView.text = getString(R.string.hi_my_name_is_with_name, name)
-    //    }
-    //}
+            nameTextView.text = getString(R.string.hi_my_name_is_with_name, name)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,24 +30,7 @@ class MainActivity : AppCompatActivity() {
         nameTextView = findViewById(R.id.name_text_view)
 
         findViewById<Button>(R.id.edit_name_button).setOnClickListener {
-            // Current implementation.. soon to be deprecated (see comment below)
-            startActivityForResult(Intent(this@MainActivity, EditTextActivity::class.java), editNameRequestCode)
-
-            // startActivityForResult will soon be deprecated and will be replaced with ActivityResult Contracts
-            // For more info, https://developer.android.com/training/basics/intents/result
-            // This is how you would launch the intent the new way
-            // startForResult.launch(Intent(this@MainActivity, EditTextActivity::class.java))
+            startForResult.launch(Intent(this@MainActivity, EditTextActivity::class.java))
         }
     }
-
-    // This callback will soon be deprecated. You'd use the code in line 19 to handle the result
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        if (requestCode == editNameRequestCode && resultCode == RESULT_OK) {
-            val name = data?.extras?.getString(INTENT_EXTRAS_KEY_NAME)  ?: getString(R.string.hi_my_name_is)
-
-            nameTextView.text = getString(R.string.hi_my_name_is_with_name, name)
-        }
-        super.onActivityResult(requestCode, resultCode, data)
-    }
-
 }
