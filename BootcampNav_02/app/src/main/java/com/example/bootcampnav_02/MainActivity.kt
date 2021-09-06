50834d7b83b3351deb27f61006e104e09c9f763d
Freddy Montano <fmontano@pinterest.com>
2021-09-06 15:19:05 -0700

    cleanup

--- BootcampNav_02/app/src/main/java/com/example/bootcampnav_02/MainActivity.kt
+++ BootcampNav_02/app/src/main/java/com/example/bootcampnav_02/MainActivity.kt
@@ -16,16 +16,6 @@ class MainActivity : AppCompatActivity() {
     private lateinit var nameTextView: TextView
     private val editNameRequestCode = 1
 
-
-    // This is how we would set the callbacks the new way
-    //private val startForResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
-    //    if (result.resultCode == Activity.RESULT_OK) {
-    //        val name = result.data?.extras?.getString(INTENT_EXTRAS_KEY_NAME) ?: getString(R.string.hi_my_name_is)
-
-    //        nameTextView.text = getString(R.string.hi_my_name_is_with_name, name)
-    //    }
-    //}
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -33,13 +23,7 @@ class MainActivity : AppCompatActivity() {
         nameTextView = findViewById(R.id.name_text_view)
 
         findViewById<Button>(R.id.edit_name_button).setOnClickListener {
-            // Current implementation.. soon to be deprecated (see comment below)
             startActivityForResult(Intent(this@MainActivity, EditTextActivity::class.java), editNameRequestCode)
-
-            // startActivityForResult will soon be deprecated and will be replaced with ActivityResult Contracts
-            // For more info, https://developer.android.com/training/basics/intents/result
-            // This is how you would launch the intent the new way
-            // startForResult.launch(Intent(this@MainActivity, EditTextActivity::class.java))
         }
     }
 
